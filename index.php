c2659cae6893edf72475fa4d62aa3a3ddea13278
Kamrul Hasan Tonmoy <141200422+Tonmoyyyy@users.noreply.github.com>
2026-05-17 18:51:29 +0600

    code modified

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['user_id']) && $_SESSION['role'] == 'restaurant_manager') {
-    header("Location: views/dashboard.php");
-} else {
-    header("Location: login.php");
-}
-exit();
-?>
